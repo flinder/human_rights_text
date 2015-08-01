9acbf33ec874c5474400ea9f4c3d3656848981f4
flinder <fridolin.linder@gmail.com>
2015-08-01 15:33:32 -0500

    various

--- code/basic_descriptives/text_descriptives_and_codes.py
+++ code/basic_descriptives/text_descriptives_and_codes.py
@@ -40,27 +40,10 @@ n_iter = df.shape[0]
 
 for idx, row in df.iterrows():
 
-
-    organization = row['Organization']
     year = row['Year']
-
-    # Resolve the country name in the info df
-    if not isinstance(row['Country'], float):
-        country = countrycode(codes = row['Country'], origin='country_name',
-                              target='country_name')
-        code = countrycode(codes = row['Country'], origin='country_name',
-                           target='iso3c')
-    else:
-        with open('log.txt', 'a') as logfile:
-            message = '[%s]: NA in country info for: %s \n' %(str(datetime.now()), row['filename'])
-            logfile.write(message)
-
-    if code is None:
-        with open('log.txt', 'a') as logfile:
-            message = '[%s]: Could not resolve country: %s \n' %(str(datetime.now()), row['filename'])
-            logfile.write(message)
-            
-    # Find matching document in databasen
+    code = row['country_iso3c']
+    
+    # Find matching document in database
     point = reports.find({'country_iso3c': code,
                           'organization': organization,
                           'year.0': year
@@ -73,45 +56,41 @@ for idx, row in df.iterrows():
             logfile.write(message)
         continue
 
-    elif point.count() > 1:
-        for subdoc in point:
-            subdoc.pop('raw_text')
-            subdoc.pop('preprocessed_text')
-            pprint(subdoc)
-    
-        error = 'multiple matches for %s %s %s \n' %(organization, year, country)
-        with open('log.txt', 'a') as logfile:
-            logfile.write(error)
 
-        continue
-    doc = next(point)
-    
-    doc['wordcount'] = row['word_count']
-    doc['country_cow_code'] = row['country_cow']
-    doc['attention'] = row['Attention']
-    doc['CIRI_codings'] = {'physical_integrity': {'killing': row['KILL'],
-                                                  'dissapearance': row['DISAP'],
-                                                  'imprisonment': row['POLPRIS'],
-                                                  'torture': row['TORT']
-                                                  },
-                            'empowerment': {'assembly': row['ASSN'],
-                                            'domestic_movement': row['DOMMOV'],
-                                            'foreign_movement': row['FORMOV'],
-                                            'speech': row['SPEECH'],
-                                            'worker_rights': row['WORKER'],
-                                            'electoral_rights': row['ELECSD'],
-                                            'religious_rights': row['NEW_RELFRE']
-                                            }                            
-                           }
-    doc['political_terror_scale'] = None
-    doc['hathaway'] = row['hathaway']
-    doc['state'] = row['State']
-    doc['genocide'] = row['genocide']
-    doc['rummel'] = row['rummel']
-    doc['massive_repression'] = row['massive_repression']
-    doc['amnesty'] = row['Amnesty']
-    doc['fariss'] = {'mean': row['latentmean'],
-                     'std_deviation': row['latentsd']
-                     }
-    reports.update({'_id': doc['_id']}, doc)
-    track_process(idx, 100, n_iter, timing = True)
+    for doc in point:
+        
+        doc['country_cow_code'] = row['country_cow']
+        doc['CIRI_codings'] = {'physical_integrity': {'killing': row['KILL'],
+                                                      'dissapearance': row['DISAP'],
+                                                      'imprisonment': row['POLPRIS'],
+                                                      'torture': row['TORT']
+                                                      },
+                                'empowerment': {'assembly': row['ASSN'],
+                                                'domestic_movement': row['DOMMOV'],
+                                                'foreign_movement': row['FORMOV'],
+                                                'speech': row['SPEECH'],
+                                                'worker_rights': row['WORKER'],
+                                                'electoral_rights': row['ELECSD'],
+                                                'religious_rights': row['NEW_RELFRE']
+                                                }                            
+                               }
+        doc['political_terror_scale'] = None
+        doc['hathaway'] = row['hathaway']
+        doc['state'] = row['State']
+        doc['genocide'] = row['genocide']
+        doc['rummel'] = row['rummel']
+        doc['massive_repression'] = row['massive_repression']
+        doc['amnesty'] = row['Amnesty']
+        doc['fariss'] = {'mean': row['latentmean'],
+                         'std_deviation': row['latentsd']
+                         }
+
+        # Insert word count
+        count = 0
+        for el in doc['preprocessed_text']:
+            count += len(el)
+        
+        doc['wordcount'] = count
+        
+        reports.update({'_id': doc['_id']}, doc)
+        track_process(idx, 9000, n_iter, timing = True)
