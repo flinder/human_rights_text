2807ad0fd0985ec022d33b9595df49d5e54092dc
flinder <fridolin.linder@gmail.com>
2015-08-01 20:37:40 +0000

    other changes on server

--- code/import_reports/import_reports.sh
+++ code/import_reports/import_reports.sh
@@ -5,4 +5,4 @@
 # logfile
 # database: name of database
 # collection: name of collection
-python import_reports.py ../../data/original_reports/ import_reports_log.txt hr_text reports
+python import_reports.py ../../data/original_reports/ import_reports_log.txt hr reports
