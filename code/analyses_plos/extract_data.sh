5ac9f6dd3533ffda93098a6d75b78840b796f7ab
flinder <fridolin.linder@gmail.com>
2015-08-02 10:24:47 -0500

    extraction from db)

--- code/analyses_plos/extract_data.sh
+++ code/analyses_plos/extract_data.sh
@@ -1,3 +1,1 @@
-# Extract the wordcounts, years and coutnry information fomr mongo db into csv file
-
-mongoexport --host 52.25.102.188 --port 27017 --db hr_text --collection reports --type=csv --fieldFile fields.txt --out ../../data/analyses_plos/all_reports.csv
+mongoexport --host 52.25.102.188 --port 27017 --db hr --collection export --type=csv --fieldFile fields.txt --out ../../data/analyses_plos/all_reports.csv
