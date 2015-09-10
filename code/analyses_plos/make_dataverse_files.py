574dfc4f575f587fcd062ec6bfa31ece60ea7aaa
flinder <fridolin.linder@gmail.com>
2015-09-10 13:20:45 -0400

    script to write clean text files for DV

--- code/analyses_plos/make_dataverse_files.py
+++ code/analyses_plos/make_dataverse_files.py
@@ -1,13 +1,22 @@
 from pymongo import MongoClient
+import re
+import os
+import codecs
 
 DB = "hr"
 COLL = "reports"
+OUTPATH = "../../data/dataverse/"
 
 connection = MongoClient()[DB][COLL]
 
 cursor = connection.find()
 
 for doc in cursor:
-	outfile_name = "{}_{}_{}.txt".format(doc["organization"], 
-					     doc["country_iso3c"],
-					     doc["year"][0])
+    outfile_name = "{}_{}_{}.txt".format(doc["organization"],
+                                         doc["country_iso3c"],
+					 doc["year"][0])
+    outfile_name = re.sub(" ", "_", outfile_name)
+    outfile_name = os.path.join(OUTPATH, outfile_name) 
+
+    with codecs.open(outfile_name, "w", "utf-8") as outfile:
+        outfile.write(unicode(doc["raw_text"])
