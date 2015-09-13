cc22d7f1d9d0f7d899ccda6a4d4ab7bae32626bd
flinder <fridolin.linder@gmail.com>
2015-09-13 17:27:38 +0000

    script for pulic data

--- code/analyses_plos/make_dataverse_files.py
+++ code/analyses_plos/make_dataverse_files.py
@@ -16,16 +16,20 @@ OUTPATH = "../../data/analyses_plos/dataverse/"
 connection = MongoClient()[DB][COLL]
 cursor = connection.find()
 
+i = 0
 for doc in cursor:
     organization = doc["organization"]
-    year = doc["year"]
+    year = doc["year"][0]
     country = doc["country_iso3c"]
     # Substitute the raw report name for the ones that did not get resolved
     if len(country) > 3:
         country = doc["report_name"]
-    outfile_name = "{}_{}_{}.txt".format(organization, year, country)
+    outfile_name = "{}_{}_{}.txt".format(country, year, organization)
     outfile_name = re.sub(" ", "_", outfile_name)
     outfile_name = os.path.join(OUTPATH, outfile_name) 
 
     with codecs.open(outfile_name, "w+", "utf-8") as outfile:
         outfile.write(unicode(doc["raw_text"]))
+    i += 1
+    if i % 100 == 0:
+	print i
