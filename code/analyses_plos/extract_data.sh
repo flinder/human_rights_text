7a7dca77a21522ebad679caf164ebd79b221c39b
flinder <fridolin.linder@gmail.com>
2015-09-27 10:24:27 -0400

    explicit host in extract data

--- code/analyses_plos/extract_data.sh
+++ code/analyses_plos/extract_data.sh
@@ -1,1 +1,1 @@
-mongoexport --db hr --collection reports --type=csv --fieldFile fields.txt --out ../../data/analyses_plos/reports_metadata.csv
+mongoexport --host=127.0.0.1 --db hr --collection reports --type=csv --fieldFile fields.txt --out ../../data/analyses_plos/reports_metadata.csv
